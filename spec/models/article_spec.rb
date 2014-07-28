253d1f53b0db4ccffcc35f31bb2687a2225f077c
Jason Draper <jason@drapergeek.com>
2014-07-28 09:47:25 -0400

    Add article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Article, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Article, :type => :model do
+  it { should validate_presence_of(:name) }
 end
